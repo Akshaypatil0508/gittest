0d5af249806ae6c0561ca00a02dda8080e3059f3
OM11_43029_akshay <akshaypatil0508@gmail.com>
2021-05-26 16:48:07 +0530

    akshay

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.*;
+
+public class MyWorld{
+ 
+  public static void main(string[] args){
+  System.out.println("Welcome to Java world");
+  System.out.println("Akshay patil");
+  }
+}
